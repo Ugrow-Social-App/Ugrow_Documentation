b67a011622b43a4fa822352bd0b0ad0009c97826
GuilhermeVeigaPedromilo <guipedromilo@gmail.com>
2026-06-27 18:09:10 -0300

    Components and Responsive Layout
    - Head, header, nav and footer like components
    - Implementing Media-Query

---
+++ src/components/navbar.js
@@ -0,0 +1,53 @@
+class Navbar extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <!-- Navbar -->
+        <nav>
+            <h3 data-i18n="navbar_title"></h3>
+            <ul class="navbar_tabs">
+                <li class="navbar_tabs_sections">
+                    <h4 data-i18n="navbar_tabs.app"></h4>
+                    <ul>
+                        <li>
+                            <a href="/" data-i18n="navbar_tabs.app.download"></a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="navbar_tabs_sections">
+                    <h4 data-i18n="navbar_tabs.documents"></h4>
+                    <ul>
+                        <li>
+                            <a href="/" data-i18n="navbar_tabs.documents.manual"></a>
+                            <a href="/" data-i18n="navbar_tabs.documents.github"></a>
+                            <a href="/" data-i18n="navbar_tabs.documents.academic"></a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="navbar_tabs_sections">
+                    <h4 data-i18n="navbar_tabs.help"></h4>
+                    <ul>
+                        <li>
+                            <a href="/" data-i18n="navbar_tabs.help.contact"></a>
+                            <a href="/" data-i18n="navbar_tabs.help.questions"></a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="navbar_tabs_sections">
+                    <h4 data-i18n="navbar_tabs.about"></h4>
+                    <ul>
+                        <li>
+                            <a href="/" data-i18n="navbar_tabs.about.app"></a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <figure>
+                <img src="./src/assets/images/logos/monochrome_logo.svg">
+                <figcaption data-i18n="navbar_figure_caption"></figcaption>
+            </figure>
+        </nav>
+     `;
+  }
+}
+
+customElements.define('navbar-component', Navbar);
--- src/js/navbar.js
+++ src/js/navbar.js
@@ -3,9 +3,7 @@ const navbar_active = () => {
     const navbar = document.querySelector("nav");
     if (navbar.classList.value == "active") {
         navbar.classList.value = "none";
-        console.log("Hello")
     } else {
         navbar.classList.value = "active";
-        console.log(navbar)
     }
 }
